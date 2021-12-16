4886b608a63f501310fa2a372d1e9bfa0bc77b9b
Donovan Rowzee <44952478+drowzee001@users.noreply.github.com>
2021-12-15 17:30:33 -0800

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://api.petfinder.com/v2',
+  apiKey: 'I4Zb17rQoF6aahCmJdP8LnlXmV0907qBHzWcEdlTZeUyQzpuMv',
+  apiSecret: 'qTWxpe47LGoQJMbJAzBuqyfAstr6PofGj1XgVwdm',
 };
