3274a7f958197186449496979058d64dd816a5b8
Donovan Rowzee <44952478+drowzee001@users.noreply.github.com>
2021-12-17 00:53:12 -0800

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,4 +3,13 @@ export const environment = {
   apiUrl: 'https://api.petfinder.com/v2',
   apiKey: 'I4Zb17rQoF6aahCmJdP8LnlXmV0907qBHzWcEdlTZeUyQzpuMv',
   apiSecret: 'qTWxpe47LGoQJMbJAzBuqyfAstr6PofGj1XgVwdm',
+  firebase: {
+    apiKey: 'AIzaSyD3ccMh7bhJqBf18rUU0CN-mZGUUMXQs3s',
+    authDomain: 'pet-utopia.firebaseapp.com',
+    projectId: 'pet-utopia',
+    storageBucket: 'pet-utopia.appspot.com',
+    messagingSenderId: '746793360112',
+    appId: '1:746793360112:web:02c7f599f382588bbd8035',
+    measurementId: 'G-49ZGD3B6TJ',
+  },
 };
